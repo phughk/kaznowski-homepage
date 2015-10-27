19f23ea7867b61cdef057faa247a579a3b3921ca
Hugh Kaznowski <hugh@geektalent.com>
2015-10-27 21:37:40 +0000

    Java tests work, filename was wrong

--- src/test/scala/com/kaznowski/hugh/homepage/ApplicationTest.scala
+++ src/test/scala/com/kaznowski/hugh/homepage/ApplicationTest.scala
@@ -2,15 +2,24 @@ package com.kaznowski.hugh.homepage
 
 import org.eclipse.jetty.server.Server
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.scalacheck.Prop.forAll
 import org.scalatest._
 import org.junit.Assert._
+import org.scalatest.junit.JUnitRunner
 
 /**
  * Created by Hugh on 26/10/2015.
  */
-class ApplicationTest extends FlatSpec {
-  @Test def isServerTest = {
+@RunWith(classOf[JUnitRunner])
+class ApplicationTest extends FunSuite with Matchers{
+
+  test("Application is instance of jetty Server") {
+    Application.isInstanceOf[Server] should be(true)
+  }
+/*
+  def isServerTest = {
     assertTrue("Application should be an instance of Jetty Server", Application.isInstanceOf[Server])
   }
+*/
 }
