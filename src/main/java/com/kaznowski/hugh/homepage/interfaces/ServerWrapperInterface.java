61936e31a65acd94feee011bdc8fc31dd1eb80ae
Hugh Kaznowski <hugh@geektalent.com>
2015-10-28 00:26:41 +0000

    Added method to ServerWrapperInterface setHandler and pom spring mvc dependancy

--- src/main/java/com/kaznowski/hugh/homepage/interfaces/ServerWrapperInterface.java
+++ src/main/java/com/kaznowski/hugh/homepage/interfaces/ServerWrapperInterface.java
@@ -6,6 +6,7 @@ import org.eclipse.jetty.server.Handler;
  * Created by hugh on 27/10/15.
  */
 public interface ServerWrapperInterface {
+
     /**
      * Set the request handler for the embedded jetty server
      * @param h the jetty handler
