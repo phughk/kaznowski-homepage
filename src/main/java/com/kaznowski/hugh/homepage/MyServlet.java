6a585f85d425fd2821bde8d81a3178167f3d997d
Hugh Kaznowski <hugh@geektalent.com>
2015-11-01 17:49:16 +0000

    Changes, don't know why it doesn't work

--- src/main/java/com/kaznowski/hugh/homepage/MyServlet.java
+++ src/main/java/com/kaznowski/hugh/homepage/MyServlet.java
@@ -6,16 +6,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 /**
  * Created by hugh on 27/10/15.
  */
 public class MyServlet extends HttpServlet {
+
+    private static final Logger logger = Logger.getLogger(MyServlet.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+        logger.info("Received get request");
         PrintWriter out = resp.getWriter();
-        resp.getWriter().write("Test 1 2 3");
+        out.write("Test 1 2 3");
+        out.flush();
+        out.close();
         resp.setStatus(200);
     }
 }
