0ac7e8114893c0199a2e4f51dd66d0bd2640ea7c
Hugh Kaznowski <hugh@geektalent.com>
2015-11-01 00:59:29 +0000

    Packages but class load conflict with jetty problem

--- src/main/java/com/kaznowski/hugh/homepage/MyServlet.java
+++ src/main/java/com/kaznowski/hugh/homepage/MyServlet.java
@@ -1,8 +1,21 @@
 package com.kaznowski.hugh.homepage;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by hugh on 27/10/15.
  */
-public class MyServlet {
-
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        resp.getWriter().write("Test 1 2 3");
+        resp.setStatus(200);
+    }
 }
--- src/test/java/com/kaznowski/hugh/homepage/MyServletTest.java
+++ src/test/java/com/kaznowski/hugh/homepage/MyServletTest.java
@@ -1,6 +1,5 @@
 package com.kaznowski.hugh.homepage;
 
-import com.kaznowski.hugh.homepage.fixtures.ServerWrapperFixture;
 import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
@@ -15,26 +14,17 @@ import java.util.List;
 public class MyServletTest {
 
     MyServlet myServlet;
-    ServerWrapperFixture serverWrapper;
     final List<String> methodsCalled = new ArrayList<>();
 
     /**
      * Needs to be initialised before the server, therefore server init calls this
      */
     public void initServerWrapper() {
-        serverWrapper = new ServerWrapperFixture();
-        serverWrapper.addMethodListener((methodName, params) -> methodsCalled.add(methodName));
     }
 
     @Before
     public void initApplication() {
         initServerWrapper();
-        myServlet = new MyServlet(serverWrapper);
-    }
-
-    @Test
-    public void applicationSetsHandler() {
-        Assert.assertTrue("The myServlet must set the handler in the constructor", methodsCalled.contains("setHandler"));
     }
 
     @After
